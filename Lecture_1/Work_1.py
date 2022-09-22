cded6c4e3d9c551ff0dbb3f6d7739eaa9f9c2d01
Marina Sudakova <rubbish_bin@inbox.ru>
2022-09-22 10:27:24 +0500

    Finish

--- Lecture_1/Work_1.py
+++ Lecture_1/Work_1.py
@@ -12,3 +12,27 @@ print(a, b, s)
 print(a, '-', b, '-', s)
 print('{1} - {0} - {2}'.format(a, b, s))
 print(f'{a} - {b} - {s}')
+
+f = True
+list = [1, 2, 3] #массив данных
+
+print('Введите a'); #ввод данных от пользователя
+a = int(input())
+
+a = int(input('a = ')) #условие
+b = int(input('b = '))
+if a > b:
+    print(a)
+else:
+    print(b)
+
+
+original = 23 #инверсия числа
+inverted = 0
+while original != 0:
+    inerted = inverted * 10 + (original % 10)
+    original // 10
+print(inverted)
+
+for i range(1, 10, 2): #цикл в порядке чисел от 1 до 9, с точкой хода 2
+    print(i)
